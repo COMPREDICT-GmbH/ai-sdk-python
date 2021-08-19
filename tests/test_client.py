0ce70e290a9e3b85ff21256614c0d7f7a257b076
Agata Rubacka <rubacka@compredict.de>
2021-08-19 16:09:15 +0200

    Add tests for client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,18 +1,21 @@
+import json
+import tempfile
+from tempfile import NamedTemporaryFile, _TemporaryFileWrapper
+
 import pytest
 
-from compredict.exceptions import Error
+from compredict.exceptions import ClientError
+from compredict.resources import Task, Algorithm
 
 
 @pytest.mark.parametrize("callback,expected",
-                        [
-                            (['firstandtheend/last', 'lifeissupereasy', 'on/island/of/trees'],
-                             "firstandtheend/last|lifeissupereasy|on/island/of/trees"),
-                            (['we/wrote2/in/symbols', 'somestateandthe/other'],
-                             "we/wrote2/in/symbols|somestateandthe/other"),
-                            ('just/a/string', 'just/a/string')
-                        ])
-
-
+                         [
+                             (['firstandtheend/last', 'lifeissupereasy', 'on/island/of/trees'],
+                              "firstandtheend/last|lifeissupereasy|on/island/of/trees"),
+                             (['we/wrote2/in/symbols', 'somestateandthe/other'],
+                              "we/wrote2/in/symbols|somestateandthe/other"),
+                             ('just/a/string', 'just/a/string')
+                         ])
 def test_set_callback_urls(api_client, callback, expected):
     actual = api_client.set_callback_urls(callback)
     assert actual == expected
@@ -32,7 +35,6 @@ def test_fail_on_error_and_verify_peer(api_client, option, expected):
 
 
 def test_last_error(api_client, response, mocker, connection, content):
-
     mocker.patch('requests.get', return_value=response)
     response = connection.GET(endpoint="some/endpoint")
     actual_last_error = api_client.last_error
@@ -41,16 +43,96 @@ def test_last_error(api_client, response, mocker, connection, content):
     assert actual_last_error == True
     assert response == False
 
-# def test_run_algorithm(api_client):
-#     algorithm_id = "id"
-#     data= {"data" : "some_data"}
-#     callback_url= ["1callback", "2callback"]
-#     callback_param = [{1:"first"}, {2:"second"}]
-#
-#     run = api_client.run_algorithm(algorithm_id=algorithm_id, data=data, callback_url=callback_url, callback_param=callback_param)
+
+def test_run_algorithm(api_client):
+    algorithm_id = "id"
+    data = {"data": "some_data"}
+    callback_url = ["1callback", "2callback"]
+    callback_param = [{1: "first"}, {2: "second"}]
+
+    api_client.run_algorithm(algorithm_id=algorithm_id, data=data, callback_url=callback_url,
+                             callback_param=callback_param)
 
 
+def test_run_algorithm_with_client_error(mocker, api_client, response_400):
+    algorithm_id = "id"
+    data = {"data": "some_data"}
+    callback_url = ["1callback", "2callback", "3callback"]
+    callback_param = [{1: "first"}, {2: "second"}]
+
+    mocker.patch('builtins.dict', side_effect=AttributeError)
+
+    with pytest.raises(ClientError):
+        api_client.run_algorithm(algorithm_id=algorithm_id, data=data, callback_url=callback_url,
+                                 callback_param=callback_param)
 
 
+def test_map_resource_with_error_raised(api_client):
+    resource = "Tassk"
+    object = {"version": "0.0.1"}
+
+    with pytest.raises(ImportError):
+        api_client._api__map_resource(resource, object)
+
+
+def test_map_resource_with_task(api_client, object):
+    resource = "Task"
+
+    instance = api_client._api__map_resource(resource, object)
+
+    assert isinstance(instance, Task)
+
+
+def test_map_resource_with_algorithm(api_client):
+    resource = "Algorithm"
+    algorithm = {
+        "id": "23",
+        "versions": [{'version': '9.4.6'}]
+    }
+    instance = api_client._api__map_resource(resource, algorithm)
+
+    assert isinstance(instance, Algorithm)
+
+
+def test_map_collection_with_raising_error(api_client, object):
+    resource = 'WrongName'
+    objects = [object, object]
+
+    with pytest.raises(ImportError):
+        api_client._api__map_collection(resource, objects)
+
+
+def test_map_collection(api_client, object):
+    resource = "Task"
+    objects = [object, object]
+
+    results = api_client._api__map_collection(resource, objects)
+
+    for result in results:
+        assert isinstance(result, Task)
+
+
+def test_process_data_with_value_error(api_client):
+
+    content_type = "text/html"
+    data = {"1": [346.5, 6456.6, 56.7], "2": [343.4, 34.6, 45.7]}
+
+    with pytest.raises(ValueError):
+        api_client._api__process_data(content_type, data)
+
+
+def test_write_json_file(api_client, data, mocker):
+
+    content_type = "application/json"
+
+    temp_file = mocker.patch('tempfile.NamedTemporaryFile')
+    mocked = mocker.patch('compredict.client.api.__write_json_file')
+
+    file, actual_content_type, bool = api_client._api__process_data(content_type, data)
+
+    assert file == temp_file
+    assert mocked.called == True
+    assert actual_content_type == content_type
+    assert bool == True
 
 
