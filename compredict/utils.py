53093b2abf956c2bedf344e0565e6fd889b682c9
Agata Rubacka <rubacka@compredict.de>
2022-02-14 09:18:45 +0100

    Make extract_error_message() utils function cleaner

--- compredict/utils.py
+++ compredict/utils.py
@@ -7,9 +7,13 @@ def extract_error_message(text: str):
     """
     Try to extract error message from AI Core 500 HTML response.
     """
-    beginning = text.find("Exception Value:")
-    end = text.find("Request information")
+    start_keyword = "Exception Value:"
+    end_keyword = "Request information"
+
+    beginning = text.find(start_keyword)
+    end = text.find(end_keyword)
+
     if beginning == -1 or end == -1:
         return "Internal Server Error"
     else:
-        return text[beginning + 17:end - 1]
+        return text[beginning + len(start_keyword) + 1:end - 1]
